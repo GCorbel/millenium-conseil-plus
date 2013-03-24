fc5158e5ce2f78852cadbb2c1faeab219be17af0
gcorbel <guirec.corbel@gmail.com>
2013-03-24 14:15:13 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConseilExpertPlus2::Application.config.secret_token = '4cab6a24e887fa1ddf323302b6f3508ac236bae72d1badbd8584a50ad7a390916188b657d2bc91a00dbd79312519a839aa7572a26d1284c37af2d3552bead787'
